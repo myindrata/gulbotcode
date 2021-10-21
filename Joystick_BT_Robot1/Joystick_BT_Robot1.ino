5a40b2d1ac66851df4148c89625837fa0ba01515
Indrata <69934537+myindrata@users.noreply.github.com>
2021-10-21 22:55:22 +0700

    Add files via upload

--- Joystick_BT_Robot1/Joystick_BT_Robot1.ino
+++ Joystick_BT_Robot1/Joystick_BT_Robot1.ino
@@ -4,58 +4,66 @@ SoftwareSerial BT(2, 3); // RX, TX
 int en[8];
 int mt[8];
 
-
-#define in1 4
-#define in2 5
-
-#define in3 6
-#define in4 7
-
-
-#define in1 8
-#define in2 9
-
-#define in3 10
-#define in4 11
-
-int xAxis, yAxis;
 int mleft, mright;
 float velo;
+String inString;
+int vx,vy,mode;
+int cnt=0;
 
 void setup() {
-  for(int i=0;i<8;i++){
-    en[i]=i+22;
-    mt[i]=i+4;
-    pinMode(en[i],OUTPUT);
-    pinMode(mt[i],OUTPUT);
-  }
-
   Serial.begin(9600);
+  Serial.println("dfdffsfds");
   BT.begin(9600); // Default communication rate of the Bluetooth module
   delay(500);
 }
 
 void loop() {
-  readbt();
-  drive(xAxis, yAxis);
+  readBTval();
+  //if (abs(vx) <=250 && abs (vy)<=250)drive(vx, vy);
+  //drive(xAxis, yAxis);
 }
 
-void readbt(){
-  if (BT.available() >= 2) {
-    xAxis = BT.read();
-    delay(5);
-    yAxis = BT.read();
-    Serial.print(xAxis);
-    Serial.print(",");
-    Serial.println(yAxis);
+void readBTval(){
+ if(BT.available()>0){
+  int inChar=BT.read();
+  inString+=(char)inChar;
+  if(inChar==';'){
+    if(cnt==0){
+      mode=inString.toInt();
+      Serial.print("mode: ");
+      Serial.print(mode);
+    }
+    if(cnt==1){
+      vx=inString.toInt();
+      Serial.print(" vx: ");
+      Serial.print(vx);
+    }
+    inString="";
+    cnt++;
   }
+  if(inChar=='\n'){
+    vy=inString.toInt();
+    Serial.print(" vy: ");
+    Serial.println(vy);
+    inString="";
+    cnt=0;
+  }
+ }
 }
-void drive(int x, int y){
-  velo=sqrt(x*x+y*y);
-  mleft=int(velo+(x/2)+0.5);
-  mright=int(velo-(x/2)+0.5);
+void drive(float x, float y){
+  velo=sqrt(x*x+y*y)/2;
+  mleft=int(velo +float(x/2)+0.5);
+  mleft=constrain(mleft,0,250);
+  mright=int(velo -float(x/2)+0.5);
+  mright=constrain(mright,0,250);
+  Serial.print(x);
+  Serial.print(";");
+  Serial.print(y);
+  Serial.print(";");
+  Serial.print(velo);
+  Serial.print(";");
   Serial.print(mleft);
-  Serial.print(",");
+  Serial.print(";");
   Serial.println(mright);
 }
 
